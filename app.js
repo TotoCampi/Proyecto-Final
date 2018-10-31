13954ab1f7df753b174c5fe01c8a4f4fd0925a90
TotoCampi <tomas.campi@ing.austral.edu.ar>
2018-10-31 13:04:48 -0300

    DB created

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+//Crea la Collection de la base de datos
+var express=require('express')
+var cors= require('cors')
+var app=express()
+const {ObjectId}= require('mongoDb');
+app.use(cors());
+app.use(express.json());
+// le digo al servidor que escuchar
+var MongoClient=require('mongodb').MongoClient;
+var url= "mongodb://localhost:27017/";
+const collectionName= 'Number List'
+// Crear la base de datos y una collection
+MongoClient.connect(url, function(err,db){
+  if (err) {
+    console.log('Error!',err)
+  }
+
+  var dbObject= db.db("mydb")
+  dbObject.createCollection(collectionName, function(err, response){
+    if (err) {
+      console.log("Error!");
+    }
+    else console.log("Collection created!")
+    // db.close() cierra la conexion
+    db.close();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
 // create a tcp modbus client
 let Modbus = require('jsmodbus');
 let net = require('net');
